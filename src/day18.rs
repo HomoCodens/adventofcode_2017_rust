ceb47aca6e8d094247c17f35024c8ad400528de8
thoenis <sthoeni@kof.ethz.ch>
2020-05-07 08:23:19 +0200

    day 18 part 2
    "lesen bildet" as they say...

--- src/day18.rs
+++ src/day18.rs
@@ -39,20 +39,25 @@ struct State {
     tx: mpsc::Sender<i64>,
     rx: mpsc::Receiver<i64>,
     part1: bool,
-    n_sent: i64
+    n_sent: i64,
+    id: i64
 }
 
 impl State {
-    fn new(tx: mpsc::Sender<i64>, rx: mpsc::Receiver<i64>, part: bool) -> State {
-        State {
+    fn new(tx: mpsc::Sender<i64>, rx: mpsc::Receiver<i64>, id: i64, part: bool) -> State {
+        let mut s = State {
             registers: HashMap::new(),
             sounded: -1,
             ptr: 0,
             n_sent: 0,
             part1: part,
             tx: tx,
-            rx: rx
-        }
+            rx: rx,
+            id: id
+        };
+
+        s.registers.insert(String::from("p"), id);
+        s
     }
 
     fn run_instruction(&mut self, instr: Instruction) -> Result<(), ()> {
@@ -64,7 +69,9 @@ impl State {
                     Value::Constant(x) => self.sounded = x,
                     Value::Register(r) => self.sounded = *self.registers.entry(r).or_insert(0)
                 }
+
                 // regressing back into clone land...
+                //println!("sending! id: {}, message: {}", self.id, self.sounded);
                 match self.tx.send(self.sounded.clone()) {
                     Ok(_) => {},
                     Err(_e) => return Err(())
@@ -72,6 +79,7 @@ impl State {
             }
             Instruction::Set{target, value} => {
                 // Eeeh...
+                //println!("Setting! id: {}, target: {:?}, value: {:?}", self.id, target, value);
                 if let Value::Register(r) = target {
                     match value {
                         Value::Constant(x) => *self.registers.entry(r).or_insert(0) = x,
@@ -83,6 +91,7 @@ impl State {
                 }
             },
             Instruction::Add{target, value} => {
+                //println!("Adding! id: {}, target: {:?}, value: {:?}", self.id, target, value);
                 if let Value::Register(r) = target {
                     match value {
                         Value::Constant(x) => *self.registers.entry(r).or_insert(0) += x,
@@ -94,6 +103,7 @@ impl State {
                 }
             },
             Instruction::Mul{target, value} => {
+                //println!("Muling! id: {}, target: {:?}, value: {:?}", self.id, target, value);
                 if let Value::Register(r) = target {
                     match value {
                         Value::Constant(x) => *self.registers.entry(r).or_insert(0) *= x,
@@ -105,6 +115,7 @@ impl State {
                 }
             },
             Instruction::Mod{target, value} => {
+                //println!("Modding! id: {}, target: {:?}, value: {:?}", self.id, target, value);
                 if let Value::Register(r) = target {
                     match value {
                         Value::Constant(x) => *self.registers.entry(r).or_insert(0) %= x,
@@ -118,21 +129,42 @@ impl State {
             Instruction::Rcv(target) => {
                 if let Value::Register(r) = target {
                     let t = self.registers.entry(r).or_insert(0);
-                    if *t > 0 {
-                        println!("toot: {}", t);
-                        match self.rx.recv() {
-                            Ok(v) => *t = v,
-                            Err(e) => return Err(())
+                    match self.rx.recv_timeout(Duration::from_secs(3)) {
+                        Ok(v) => {
+                            //println!("Having received! id: {}, message: {}", self.id, v);
+                            *t = v;
+                        },
+                        Err(_e) => {
+                            println!("Stopping due to receive error! id: {}", self.id);
+                            return Err(());
                         }
                     }
                 }
             },
             Instruction::Jgz{register, offset} => {
-                if let Value::Register(r) = register {
-                    if *self.registers.entry(r).or_insert(0) > 0 {
-                        match offset {
-                            Value::Constant(x) => self.ptr += x as usize - 1,
-                            Value::Register(r2) => self.ptr += *self.registers.entry(r2).or_insert(0) as usize - 1
+                //println!("JGZing! id: {}, register: {:?}, offest: {:?}", self.id, register, offset);
+                // Thanks @vash3r lol
+                // https://www.reddit.com/r/adventofcode/comments/7kj35s/2017_day_18_solutions/
+                match register {
+                    Value::Register(r) => {
+                        let &mut register_value = self.registers.entry(r).or_insert(0);
+                        if register_value <= 0 {
+                            //println!("also, I'm not going anywheres!")
+                        }
+                        if register_value > 0 {
+                            match offset {
+                                Value::Constant(x) => self.ptr += x as usize - 1,
+                                Value::Register(r2) => self.ptr += *self.registers.entry(r2).or_insert(0) as usize - 1
+                            }
+                        }
+                    },
+                    Value::Constant(x) => {
+                        // This code is very WET (we eternally t...uplicate?)
+                        if x > 0 {
+                            match offset {
+                                Value::Constant(x) => self.ptr += x as usize - 1,
+                                Value::Register(r2) => self.ptr += *self.registers.entry(r2).or_insert(0) as usize - 1
+                            } 
                         }
                     }
                 }
@@ -194,7 +226,7 @@ fn day18_part1(instructions: &Vec<Instruction>) -> i64 {
 
     let (tx, rx) = mpsc::channel();
 
-    let mut s = State::new(tx, rx, true);
+    let mut s = State::new(tx, rx, 0, true);
 
     loop {
     //for _k in (0..10) {
@@ -214,20 +246,53 @@ fn day18_part1(instructions: &Vec<Instruction>) -> i64 {
     s.sounded
 }
 
+#[aoc(day18, part2)]
+fn day18_part2(instructions: &Vec<Instruction>) -> i64 {
+    let (tx_a, rx_a) = mpsc::channel();
 
+    let (tx_b, rx_b) = mpsc::channel();
 
-fn main() {
-    thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
+    let mut a = State::new(tx_a, rx_b, 0, false);
+    let mut b = State::new(tx_b, rx_a, 1, false); // Be nice and say hello to each other...
+
+    let instrs_a = instructions.clone();
+
+    let instrs_b = instructions.clone();
+
+    let thingamabob = thread::spawn(move || {
+        println!("I am a thread and you are still (somewhat) sane");
+        loop {
+            if a.ptr >= instrs_a.len() {
+                println!("program 0 exiting due to pointer out of range");
+                break;
+            }
+            let i = instrs_a[a.ptr].clone();
+            match a.run_instruction(i) {
+                Ok(_) => {},
+                Err(_) => break
+            }
             thread::sleep(Duration::from_millis(1));
         }
+        println!("banana for the munkey");
+        println!("{}", a.n_sent);
     });
 
-    for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
+    loop {
+        if b.ptr >= instrs_b.len() {
+            println!("program 1 exiting due to pointer out of range");
+            break;
+        }
+        let i = instrs_b[b.ptr].clone();
+        match b.run_instruction(i) {
+            Ok(_) => {},
+            Err(_) => break
+        }
         thread::sleep(Duration::from_millis(1));
     }
+
+    thingamabob.join();
+
+    b.n_sent
 }
 
 #[cfg(test)]
@@ -259,6 +324,100 @@ set a 0
 rcv a
 jgz a -1
 set a 1
-jgz a -2")), 2);
+jgz a -2")), 4);
+    }
+
+    #[test]
+    fn day18ex2() {
+        assert_eq!(day18_part2(&parse_instructions(&"snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d")), 5);
     }
 }
+
+/*
+Figuring out what it does like a pedestrian
+
+# init
+set i 31
+set a 1
+
+# If you are program 1, GOTO @ENDFOR
+mul p 17
+jgz p p
+
+# Set a to 2^31 - 1 (or thereabouts)
+mul a 2
+add i -1
+jgz i -2
+add a -1
+
+# 127 times
+@BEGINFOR
+set i 127
+# Set seed
+set p 826
+
+# b = (((8505*p % a)*129749 + 12345) % a) % 10000
+# i.e. some pseudorandom value
+mul p 8505
+mod p a
+mul p 129749
+add p 12345
+mod p a
+set b p
+mod b 10000
+
+# send b
+snd b
+
+// decrease i and jgz to @BEGINFOR
+add i -1
+jgz i -9
+
+@ENDFOR
+
+# if a is zero, receive until you get zero
+jgz a 3
+rcv b
+jgz b -1
+
+# else 
+# init
+set f 0
+set i 126
+
+# get 2 values
+rcv a
+@BLA
+rcv b
+
+# if b > a GOTO @CASE1 else GOTO @CASE2
+set p a
+mul p -1
+add p b
+jgz p 4
+
+# Send a (i.e. the larger value)
+snd a
+# keep the smaller value
+set a b
+jgz 1 3
+
+@CASE1
+snd b
+# Sorting not done (at least one larger value received after a smaller one)
+set f 1
+
+# if not done looping, go to @BLA (receive a new b)
+add i -1
+jgz i -11
+snd a
+jgz f -16
+# go lock up?
+jgz a -19
+*/
